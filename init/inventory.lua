dc037d7e199819dc09e9bb08495366aab85e73ac
André Carlos <andre-carlos@live.com>
2024-01-18 23:08:28 -0300

    removing comments

--- init/inventory.lua
+++ init/inventory.lua
@@ -24,7 +24,6 @@ function init_inv()
             bul.active=false
         end
 
-        -- se sim, move o projétil de acordo com sua direção e velocidade
         if bul.dir == "left" then
             bul.x = bul.x - bul.spd
         elseif bul.dir == "right" then
@@ -35,16 +34,14 @@ function init_inv()
             bul.y = bul.y + bul.spd
         end
            
-        local tile = mget(flr(bul.x / 8), flr(bul.y / 8)) -- obtém o tile na posição do projétil
-        local flag = fget(tile, 0) -- obtém o valor do flag 0 do tile
+        local tile = mget(flr(bul.x / 8), flr(bul.y / 8))
+        local flag = fget(tile, 0) 
         if bul.x < 0 or bul.x > 127 or 
             bul.y < 0 or bul.y > 127 or flag then
-            -- se sim, desativa o projétil
             bul.x = -100
         end
-        for enemy in all(enmies) do -- percorre todos os inimigos na tabela
-            if collide(bul, enemy) then -- verifica se o projétil colidiu com o inimigo
-              -- se sim, faz alguma ação, como reduzir a vida do inimigo, tocar um som, etc.
+        for enemy in all(enmies) do 
+            if collide(bul, enemy) then 
               del(enmies, enemy)
               bul.x=-100
             end
